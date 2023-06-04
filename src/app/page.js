483b6ce12e81397e25eff87c4b4b51c6c689f65e
Andriikryl <andreyk557@gmail.com>
2023-06-04 19:36:31 +0300

    add project list/title

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,63 @@
 import styles from "./page.module.css";
 
+const projects = [
+  {
+    title: "New York",
+    color: "#F06318",
+    id: 1,
+  },
+  {
+    title: "San Francisco",
+    color: "#DCF018",
+    id: 2,
+  },
+  {
+    title: "San Antonio",
+    color: "#18F0E8",
+    id: 3,
+  },
+  {
+    title: "Nashville",
+    color: "#8C0CF0",
+    id: 4,
+  },
+  {
+    title: "Houston",
+    color: "#F0183C",
+    id: 5,
+  },
+  {
+    title: "New Orleans",
+    color: "#F0BA18",
+    id: 6,
+  },
+  {
+    title: "Oklahoma City",
+    color: "#0C34F0",
+    id: 7,
+  },
+  {
+    title: "Detroit",
+    color: "#0CBCF0",
+    id: 8,
+  },
+  {
+    title: "Indianapolis",
+    color: "#91F018",
+    id: 9,
+  },
+];
+
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      {projects.map((project, index) => {
+        return (
+          <div key={project.id}>
+            <p>{project.title}</p>
+          </div>
+        );
+      })}
+    </main>
+  );
 }
